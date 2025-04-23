233a6852e44c1076f84a2ce1cba7ea6c7fcb3a43
Diego Leonardo Alejo Cantu <angus20@192.168.1.12>
2025-04-22 21:51:07 -0600

    Proyecto en proceso . . .

---
+++ src/main/java/com/java/farmacia_alejo/FarmaciaAlejoApplication.java
@@ -0,0 +1,13 @@
+package com.java.farmacia_alejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaAlejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaAlejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/farmacia_alejo/FarmaciaAlejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.farmacia_alejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaAlejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
